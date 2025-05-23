3a081c5943a13096cad485382398d8414568215b
gigi-ashly <ashlymargra@gmail.com>
2025-05-23 13:07:59 -0600

    Create Martinez_Ashly_Exam1.java

---
+++ src/semana_5_examen_i/Martinez_Ashly_Exam1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana_5_examen_i;
+
+/**
+ *
+ * @author Kurtg
+ */
+public class Martinez_Ashly_Exam1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
